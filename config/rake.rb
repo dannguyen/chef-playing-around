264d859070138b827dc4e8547390c6c5ee756663
Dan Nguyen <dansonguyen@gmail.com>
2014-04-13 22:03:24 -0400

    s

---
+++ config/rake.rb
@@ -0,0 +1,6 @@
+# Configure the Rakefile's tasks.
+
+# Not currently used...
+
+# in the rails-quick-start, this is where
+# ssl_cert is generated
---
+++ todo.md
@@ -0,0 +1,6 @@
+## Looking over rails-quick-start
+
+
+- Checkout the aws recipe
+- add open ssl dependency
+
